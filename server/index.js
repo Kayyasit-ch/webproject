59e7e9501f1404538769221a6a0508d049436972
Kayyasit-ch <kayyasit.ch@gmail.com>
2025-04-24 14:02:54 +0700

    api and start frontend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const mysql = require('mysql2');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 const db= mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -12,7 +14,7 @@ const db= mysql.createConnection({
 
 }).promise();
 
-app.get('/api/product' ,async (req,res) =>{
+app.get('/api/products' ,async (req,res) =>{
   try
   {
   const [row] = await db.query('SELECT * FROM products_project');
@@ -22,6 +24,85 @@ app.get('/api/product' ,async (req,res) =>{
   }
 });
 
+app.post('/api/add', async (req, res) => {
+  try {
+    const { codeproduct, name, category, price, date, piece } = req.body;
+
+    if (!codeproduct || !name || !category || !price || !date || !piece) {
+      return res.status(400).json({ message: 'press enter your product' });
+    }
+
+    const [result] = await db.query(
+      'INSERT INTO products_project (codeproduct, name, category, price, date, piece) VALUES (?, ?, ?, ?, ?, ?)',
+      [codeproduct, name, category, price, date, piece]
+    );
+
+    res.status(201).json({ message: 'Product added', id: result.insertId });
+  } catch (err) {
+    console.error('Error adding product:', err);
+    res.status(500).json({ message: 'Error added product', error: err.message });
+  }
+});
+
+app.get('/api/product/:codeproduct' , async (req,res) => {
+  try{
+    const {codeproduct} =req.params
+    const [rows] =await db.query('SELECT * FROM products_project WHERE codeproduct = ?', [codeproduct]);
+    if (rows.length === 0) {
+      return res.status(404).json({ message: 'Product not found' });
+    }
+    res.status(200).json(rows);
+  }
+  catch(err){
+    res.status(500).json({message:'error fach',error:err.message});
+  }
+  
+});
+
+app.put('/api/update/:id' , async (req,res) => {
+  try{
+    const {id}=req.params
+    const { codeproduct, name, category, price, date, piece } = req.body;
+    
+    if (!codeproduct || !name || !category || !price || !date || !piece) {
+      return res.status(400).json({ message: 'press enter your product' });
+    }
+
+    const [result] = await db.query(
+      'UPDATE products_project SET codeproduct = ?,name = ?, category = ?, price = ?, date = ?,piece = ?  WHERE id=?',[codeproduct, name, category, price, date, piece,id]
+    );
+
+    if (result.affectedRows === 0){
+      return res.status(404).json({message : 'Product not found'});
+    }
+    res.status(200).json({message: 'Update product sucessfully'});
+
+  }
+  catch(err){ 
+    res.status(500).json({message: 'Erorr update product ' ,error:err.message});
+  }
+  
+});
+
+app.delete('/api/delete/:id' , async (req,res) => {
+  try{
+    const {id}=req.params
+    const[result]=await db.query('DELETE FROM products_project WHERE id = ? ',[id]);
+    if (result.affectedRows === 0){
+      return res.status(404).json({message : 'Product not found'});
+    }
+    res.status(200).json({message: 'Delete product sucessfully'});
+
+
+  }
+  catch(err){
+    res.status(500).json({message: 'Error delete product', error:err.message});
+
+  }
+  
+});
+
+
 app.listen(port, ()=>{
   console.log(`server running http://localhost:${port}`)
-})
+});
